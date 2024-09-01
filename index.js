68fa7c357f39069aba59efcc58e6a192925e5ce5
amenaparvin <amenaparvin2284@gmail.com>
2024-09-02 00:17:45 +0600

    for each

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let archetype=["Android","IOS","Windows","Mac","Web"]
+/*let archetype=["Android","IOS","Windows","Mac","Web"]
         let city=["Dhaka","Rasshahi","Rangpur"]
         let numbers=[1,40,50,60,3,2]
 
-        console.log(archetype)
+        console.log(archetype)*/
 
+
+
+let archetype=["Android","IOS","Windows","Mac","Web"]
+
+
+archetype.forEach((item)=>{
+    console.log(item)
+
+})
