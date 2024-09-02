96acf8a999a861e11fc07015234631e093676b47
amenaparvin <amenaparvin2284@gmail.com>
2024-09-02 15:55:59 +0600

    JS Math

--- index.js
+++ index.js
@@ -142,5 +142,22 @@ console.log(peoples[1] ['age'])
 
 //----------------------date object-------------------
 
-let date=new Date()
-console.log(date)
+/*let date=new Date()
+console.log(date.getMonth())
+*/
+
+//----------------------JS Math-------------------
+
+
+//abs,ceil,floor,max,min,random,round,PI,pow
+
+console.log(Math.abs(-90))
+console.log(Math.PI)
+console.log((1000+Math.random()*9000))
+console.log(Math.pow(2,3))
+console.log(Math.sqrt(16))
+console.log(Math.min(1,2,3))
+console.log(Math.max(1,2,3))
+console.log(Math.round(4,7))
+console.log(Math.ceil(4,7))
+console.log(Math.floor(4,7))
